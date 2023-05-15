44aaf7bab667a765f00e1fc090c628e11a2a8221
AdverseMiller <ajmiller0515@gmail.com>
2023-05-15 17:24:01 -0500

    update offsets

--- src/Offsets.cpp
+++ src/Offsets.cpp
@@ -5,13 +5,13 @@ namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x07871c50;            // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x074034b0;            // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;              // [Mine]          -> Region
-    const long LEVEL = 0x1639960;                 // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = (0x020c1130 + 0x8); // [Miscellaneous] -> AVC_GameMovement+0x8
-    const long ENTITY_LIST = 0x1d0fe58;           // [Miscellaneous] -> cl_entitylist
+    const long LEVEL = 0x165bd30;                 // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = (0x02139480 + 0x8); // [Miscellaneous] -> AVC_GameMovement+0x8
+    const long ENTITY_LIST = 0x1d87e88;           // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
@@ -23,12 +23,12 @@ namespace offsets
     const long TEAM_NUMBER = 0x044c;                // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
     const long NAME = 0x0589;                       // [RecvTable.DT_BaseEntity]        -> m_iName
     const long LIFE_STATE = 0x0798;                 // [RecvTable.DT_Player]            -> m_lifeState
-    const long VEC_PUNCH_WEAPON_ANGLE = 0x24b0;     // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-    const long VIEW_ANGLE = (0x25ac - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
-    const long BLEEDOUT_STATE = 0x2740;             // [RecvTable.DT_Player]            -> m_bleedoutState
-    const long ZOOMING = 0x1c51;                    // [RecvTable.DT_Player]            -> m_bZooming
-    const long LAST_VISIBLE_TIME = 0x1a78;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
+    const long VEC_PUNCH_WEAPON_ANGLE = 0x24c0;     // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+    const long VIEW_ANGLE = (0x25bc - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
+    const long BLEEDOUT_STATE = 0x2750;             // [RecvTable.DT_Player]            -> m_bleedoutState
+    const long ZOOMING = 0x1c61;                    // [RecvTable.DT_Player]            -> m_bZooming
+    const long LAST_VISIBLE_TIME = 0x1A80;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
     const long CURRENT_SHIELDS = 0x0170;            // m_shieldHealth
     const long CURRENT_HEALTH = 0x043c;		    // m_iHealth
-    const long ARMOR_TYPE = 0x4634;		    // m_armorType
+    const long ARMOR_TYPE = 0x4634;
 }
